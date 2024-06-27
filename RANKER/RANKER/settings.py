5dfa22b2b954c6ce730a527cf43471aa6ee26a35
Kevin <kaunghtetsan6574@gmail.com>
2024-06-26 23:20:41 -0700

    whitenoise

--- RANKER/RANKER/settings.py
+++ RANKER/RANKER/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'rankings/static'),]
-STATICFILES_STORAGE = whitenoise.storage.CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
