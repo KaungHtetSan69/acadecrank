aa038ec3a315230e2a8228ee1fc94adc022cec3c
Kevin <kaunghtetsan6574@gmail.com>
2024-06-29 11:53:38 -0700

    postgres

--- RANKER/RANKER/settings.py
+++ RANKER/RANKER/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'RANKER.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TCesIionXxfKnybcYjHMVDgDvBuvgmLb',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '32704',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
