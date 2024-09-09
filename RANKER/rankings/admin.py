523a3f50435e05fdcbea1576ad5714be6ff14861
Kevin <kaunghtetsan6574@gmail.com>
2024-09-08 21:41:52 -0700

    admin adding imprompt

--- RANKER/rankings/admin.py
+++ RANKER/rankings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from rankings.models import Student, aevent, input
+from rankings.models import Student, aevent, input,impromptu_questions
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(aevent)
 admin.site.register(input)
+admin.site.register(impromptu_questions)
